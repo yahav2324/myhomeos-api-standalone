941424670aaca2362e4e9cbe45636aea5ae1ebe7
yahav <yahav2324@gmail.com>
2026-02-26 22:50:12 +0200

    fix add prisma.config

--- prisma.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "@prisma/config";
-
-export default defineConfig({
-  datasource: {
-    // בשימוש ב-Prisma 7, אנחנו מכוונים ל-DIRECT_URL עבור פעולות Migrate
-    url: process.env.DIRECT_URL || process.env.DATABASE_URL,
-  },
-});
